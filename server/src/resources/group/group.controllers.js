eede00f9fc47a22f8af0eccc8bc93810c74893a5
santt31 <santt31@gmail.com>
2022-07-05 18:32:17 +0200

    transaction issue

--- server/src/resources/group/group.controllers.js
+++ server/src/resources/group/group.controllers.js
@@ -39,21 +39,21 @@ const createOne = async (req, res)=>{
 }
 
 const createGroupTran = async (req,res)=>{
-    try {
-
+    const session = await mongoose.startSession();
+    await session.startTransaction( async ()=>{
         //PASO 0- El usuario a quien voy agregar al grupo tiene que estar creado
         //PASO 1- Creo el grupo
         const newUser = req.body;
-        const groupNew = await Group.create(newUser);
+        const groupNew = await Group.create([newUser], 
+            {session});
+        console.log('groupNew es ', groupNew);
 
-        //PASO 1.1 - Crea o se envia el array de usuarios agregar
-        //el mismo que crea y un amigo (Erika y Santiago), lo determina FRONT
-        //const arrayUsers = ['62c304cb21b02466c5c2065a','62b5e88ba6e78636d6488645']
-
-
+        return groupNew;
+        
         //PASO 2 - Actualizo user
         //A todos esos usuarios del array del objeto, le agrego a cada uno el id del grupo
-        const doc = await User.updateMany({
+        /*const doc = await User.updateMany(
+        {
             _id: {
                 $in: req.body.users
             }
@@ -64,14 +64,24 @@ const createGroupTran = async (req,res)=>{
             }
         },
         {multi: true}
-        );
-       
-        if(!doc){
+        , {session});*/
+        
+        /*if(!groupNew){
             return res.status(404).json({ error : "Not found"});
         }
-        res.status(200).json({ results: [doc]});
+        return res.status(200).json({ results: [groupNew]});*/
+    });
+    
+    
+    try {
+       
+     
+        session.commitTransaction();
+        session.endSession();
 
     } catch (error) {
+        await session.abortTransaction();
+
         console.log(error);
         res.status(500).json({ error: 'Cannot update'});
     }
