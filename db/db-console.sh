6cac1477fa2bb3e883ca57169bc86faa2a785d1f
santt31 <santt31@gmail.com>
2022-07-06 18:27:30 +0200

    second option to connect mongoose and replica

--- db/db-console.sh
+++ db/db-console.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker exec -it mongo1 mongo -u admin -p fullstack
+docker exec -it mongo1 mongo
 
 # docker exec -it db-split-app-mongo-1 mongosh -u admin -p fullstack
--- server/src/db.js
+++ server/src/db.js
@@ -1,9 +1,25 @@
 const mongoose = require('mongoose');
 const { MONGO_URL } = require('./config');
 
+//NO ESTA PROBADO
+const options ={
+    server: {
+        socketOptions:  {keepAlive: 1},
+        readPreference: "nearest",
+        strategy: "ping"
+    },
+    replset: {
+        rs_name: 'ReplicaSet',
+        socketOptions:  {keepAlive: 1},
+        strategy: 'ping',
+        readPreference: 'nearest',
+        poolSize: 10
+    }
+}
+
 const connect = async() =>{
     try {
-        await mongoose.connect(MONGO_URL);
+        await mongoose.connect(MONGO_URL, options);
         console.log("Mongoose connected");
     } catch (error) {
         console.error(`Couldn't connect to MongoDB with Mongoose: ${error}`);
