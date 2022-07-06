f9a005b05a24a65a1f73ee6d431935b56f5c9c4e
santt31 <santt31@gmail.com>
2022-07-06 17:24:23 +0200

    docker replicas

--- db/db-console.sh
+++ db/db-console.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-docker exec -it db-split-app-mongo-1 mongosh -u admin -p fullstack
+docker exec -it mongo1 mongo -u admin -p fullstack
+
+# docker exec -it db-split-app-mongo-1 mongosh -u admin -p fullstack
