57e9202e318d0e12d66fa23b49bc02d4f0881279
santt31 <santt31@gmail.com>
2022-07-11 16:31:58 +0200

    fix

--- server/src/resources/group/group.route.js
+++ server/src/resources/group/group.route.js
@@ -2,11 +2,10 @@ const { Router } = require("express");
 const GroupControllers = require("./Group.controllers");
 const router = Router();
 
-router.post('/', GroupControllers.createOne);
 router.get('/', GroupControllers.findMany);
 router.get('/:id', GroupControllers.findOne);
 router.put('/:id', GroupControllers.updateOne);
-router.put('/', GroupControllers.createGroupUser);
+router.post('/', GroupControllers.createGroupUser);
 
 router.delete('/:idUser', GroupControllers.deleteOne);
 
