ec4e54213c39010ea6b16d1c33c81e0c2c685728
Ramiro Mejías Rodríguez <ramiro.mejias@gmail.com>
2022-08-07 08:12:46 -0700

    Friend resend invitation endpoint created

--- server/src/resources/email/email.controllers.js
+++ server/src/resources/email/email.controllers.js
@@ -70,6 +70,53 @@ const sendInvitation = async (req, res) => {
   }
 };
 
+const resendInvitation = async (req, res) => {
+  const { email, authUser } = req.body;
+
+  try {
+    // Buscar datos del amigo
+    const friendExist = await User.findOne(
+      { _id: authUser, "friends.friendEmail": email },
+      { _id: 0, name: 1, friends: { $elemMatch: { friendEmail: email } } }
+    );
+    if (!friendExist) {
+      console.log("Friend does not exist");
+      return res.status(500).json({ error: "Friend does not exist" });
+    }
+
+    const message = `<h2>Shared Expenses Invitation</h2>
+    <p>Hello, ${friendExist.friends[0].friendName}</p>
+    <p>Your friend ${friendExist.name} has invited you to share expenses in an easy way through Shared Expenses, 
+    for this you can:</p>
+    <ol>
+    <li>Download the application<br />
+    <strong>Android: </strong>https://play.google.com/store/apps/details?id=com.SharedExpenses.SharedExpensesMobile<br />
+    <strong>iOS: </strong>https://apps.apple.com/us/app/sharedexpenses/id512463895</li>
+    <li>Register using this link<br />https://wwww.sharedexpenses.com/register?friend=${friendExist.friends[0].invitationId}</li>
+    <ol>`;
+
+    // Enviar email de invitacion
+    const send = await transporter.sendMail({
+      from: '"Shared Expenses" <sharedexpenses.europe@gmail.com>', // remitente
+      to: email, // destinatario
+      subject: "Shared Expenses Invitation", // asunto
+      html: message, // cuerpo
+    });
+    if (!send) {
+      console.log("Error sending email");
+      return res
+        .status(500)
+        .json({ error: "There was an errors sending email" });
+    }
+
+    res.status(200).json({ message: "Invitation sent successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Errors reinviting friends" });
+  }
+};
+
 module.exports = {
   sendInvitation,
+  resendInvitation,
 };
--- server/src/resources/email/email.route.js
+++ server/src/resources/email/email.route.js
@@ -3,5 +3,6 @@ const emailControllers = require("./email.controllers");
 const router = Router();
 
 router.post("/invitation", emailControllers.sendInvitation);
+router.post("/reinvitation", emailControllers.resendInvitation);
 
 module.exports = router;
