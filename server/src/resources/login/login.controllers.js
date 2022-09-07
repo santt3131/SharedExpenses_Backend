4dd84d6a7b9041f5c2ea8fcb80d4d54f11ad608b
AB <boudjemaaabdelwahed@gmail.com>
2022-09-07 18:49:21 +0200

    reset pwd v4

--- server/src/resources/login/login.controllers.js
+++ server/src/resources/login/login.controllers.js
@@ -2,31 +2,26 @@ const User = require("../user/user.model");
 const userServices = require("../user/user.service");
 const Auth = require("../user/user.service");
 const { needsAuthToken } = require("../user/auth/auth.middleware");
+//import * as tk from './token';
 
 
 const login = async (req, res) => {
   const loginData = req.body;
-/*
-if(loginData)
-{
-  const token = await userServices.authenticateUser(loginData);
-  
-  if(token){
-  res.status(200).json(token);
-  return token;
-  }
-  else {console.log('111111'); return"err";}
-
-}
-else
-{
-  console.log(e);
-  return "worong email or password";
-}
-*/
   try {
     const loginValidation = await userServices.authenticateUser(loginData);
     res.status(200).json(loginValidation);
+  } catch (e) {
+    
+    console.log(e);
+  }
+
+};
+  const logOut = async (req, res) => {
+  const [token, setToken] = useState(tk.readToken);
+  const SignOut = req.body;
+  try {
+    const SignOut = await userServices.authenticateUser(loginData);
+    res.status(200).json(SignOut);
   } catch (e) {
     
     console.log(e);
